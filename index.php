446592ed5400e5dceb51acacd893da67b3c397bd
Muhammad Zeeshan Latif <zeeshanmuhammad42201@gmail.com>
2023-10-31 22:50:19 +0500

    Index

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+ $name="Zeeshan";
+$age=25;
+$isMarried=false;
+$marks=84.25;
+echo "My name is " .$name ."<br>";
+echo "My age is " .$age ."<br>";
+echo "My marks are " .$marks ."%<br>";
+echo "Merital status " .$isMarried ."<br>";
+//  check data type in php
+function myTest(){
+ $name;
+echo $name;
+echo $name;
+
+}
+myTest();
+?>
